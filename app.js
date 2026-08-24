2e561f8ca9b1baefde8894984c69568731f30511
Prajwal <prajwal.kulkarni@bhartisofttech.com>
2026-08-24 16:17:36 +0530

    added login functonality

--- app.js
+++ app.js
@@ -3,3 +3,12 @@ console.log("Hello world!");
 function greet(name) {
     console.log("Hello", name);
 }
+
+function login({ name, password }) {
+    // network calls with required data i.e. name & password
+
+    // response
+
+    // success -> redirect to /home page
+    // failure -> error show
+}
